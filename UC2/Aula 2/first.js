bf959fa5de2d4ec239ad29be3cca345facd36d2b
William Belhome <145597088+wbelhome@users.noreply.github.com>
2026-03-24 21:22:51 -0300

    Add files via upload

---
+++ UC2/Aula 2/first.js
@@ -0,0 +1,40 @@
+// exercicio 1
+// let nome = "William"
+// let sobrenome = "Belhome"
+// let idade = 27
+// let estudante = true
+
+// if( estudante == true){
+//     console.log(`Olá ${nome} ${sobrenome} eu tenho ${idade} anos e sou estudante!`)
+// }
+// else{
+//     console.log(`Olá ${nome} ${sobrenome} eu tenho ${idade} anos e não sou estudante!`)
+// }
+
+
+// Teste de verificação de undefined e null
+// let variavel
+// console.log(variavel)
+// variavel = null 
+// console.log(variavel)
+
+// Conversão de Number para String 
+// const idade = 24
+// const ageText = idade.toString()
+//  console.log(typeof idade);
+//  console.log(typeof ageText);
+// Conversão de String para Number
+// const ageText = "23"
+// const age = Number(ageText)
+// console.log(typeof ageText)
+// console.log(typeof age)
+
+// Exercicio 2 
+
+const idade = 24
+const ageText = idade.toString()
+console.log(typeof idade);
+console.log(typeof ageText);
+console.log(ageText);
+
+ 
