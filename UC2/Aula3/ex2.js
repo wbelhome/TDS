b32494eae60487c6828bd41978f2d57c361291f8
wbelhome <william.belhome99@gmail.com>
2026-03-27 13:10:04 -0300

    up

--- UC2/Aula3/ex2.js
+++ UC2/Aula3/ex2.js
@@ -6,9 +6,28 @@
 // console.log(num > num2)
 // console.log(num < num2)
 
-let a = true
-let b = false
-let c = true
+// let a = true
+// let b = false
+// let c = true
 
-console.log(a && b && c)
+// console.log(a && b && c)
+
+const nome = "Laís"
+let yearB = 2015
+let age = 2026 - yearB
+let ageO = 2050 - yearB 
+let maior
+
+if(age >= 18){
+    maior = true
+    console.log(`Meu nome é ${nome} minha idade é ${age} anos, sou maior de idade e em 2050 terei ${ageO}`)
+}else{
+    maior = false
+    console.log(`Meu nome é ${nome} minha idade é ${age} anos, sou menor de idade e em 2050 terei ${ageO}`)
+}
+
+ 
+
+
+
 
