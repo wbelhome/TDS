9ecdd960cc72f63014242c91de72b9df9eab99e0
wbelhome <william.belhome99@gmail.com>
2026-05-04 11:40:38 -0300

    update trabalho

--- UC2/Aula6/trabaio.js
+++ UC2/Aula6/trabaio.js
@@ -2,14 +2,14 @@ const prompt = require('readline-sync');
 const usuarios = [];
 function main(){
     while(true){
-        const resposta = prompt.question(`
+        const resposta = Number(prompt.question(`
         Escolha uma opcao: \n
         1. Criar usuario\n
         2. Ver relatorio\n
         3. Descrever usuario\n
         4. Sair\n
-        `).toLowerCase();
-        if(resposta === '1'){
+        `));
+        if(resposta === 1){
         const name = prompt.question("Digite seu nome: ");
         const age = prompt.question("Digite sua idade: ");
         const city = prompt.question("Digite sua cidade: ");
@@ -19,16 +19,17 @@ function main(){
         
         makeUser(name, age, city, hobbie, hobbie2, hobbie3 );
         
-        }else if(resposta === '2'){
+        }else if(resposta === 2){
             console.log(relatorio(usuarios));
-        }else if(resposta === '3'){
-            const i = prompt.question("Digite o indice do usuario que deseja descrever: ");
+        }else if(resposta === 3){
+            let reposta = Number(prompt.question("Digite o numero do usuario que deseja descrever: "))
+            let i = reposta - 1;
             if(i >= 0 && i < usuarios.length){
                 console.log(description(usuarios[i]));
             }else{
                 console.log("indice invalido.");
             }
-        }else if(resposta === '4'){
+        }else if(resposta === 4){
             console.log("Ok, até a proxima!");
             break;
         }else{
@@ -51,29 +52,35 @@ function makeUser(name, age, city, hobbie, hobbie2, hobbie3){
     const usuario = [...user, hobbies]
     formatarHobbies(usuario);
     addUser(usuarios, usuario);
+
+    console.log(`Usuario ${name} criado com sucesso!`);
 }
 
 function formatarHobbies(usuario){
-    const [name, age, city, hobbies] = usuario;
+    const [ , , , hobbies] = usuario;
     const hobbieFormatado =  `${hobbies[0]}, ${hobbies[1]} e ${hobbies[2]}`;
     return hobbieFormatado;
 }
 
 function description(usuario){
-    const [name, age, city] = usuario;
-    const hobbieFormatado = formatarHobbies(usuario);
-    return `Olá, meu nome é ${name}, tenho ${age} anos, moro em ${city} e meus hobbies são ${hobbieFormatado}`
+    const [name, age, city, hobbies] = usuario;
+    return `Olá, meu nome é ${name}, tenho ${age} anos, moro em ${city} e meus hobbies são ${formatarHobbies(usuario)}`
 }
 
 
 function addUser(usuarios, usuario){
     usuarios.push(usuario);
-    console.log(usuarios);
+    return usuarios;
 }
 
 function relatorio(usuarios){
     const quantidadeUsuarios = usuarios.length;
     console.log(`Quantidade de usuários: ${quantidadeUsuarios}`);
+    
+    for(let i = 0; i < usuarios.length; i++){
+        console.log(`${i + 1}. ${usuarios[i][0]}`);
+    }
 }
+
 
 
