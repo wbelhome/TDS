2c27409f48acaf8ff2db2668c7a78617813394eb
wbelhome <william.belhome99@gmail.com>
2026-05-05 19:18:09 -0300

    up

--- UC2/Aula6/teste.js
+++ UC2/Aula6/teste.js
@@ -1,44 +1,86 @@
-function criarUsuario(name, age, city, hobbies){
-    return [name, age, city, hobbies];
-}
-
-function formatarHobbies(hobbies){
-    return `${hobbies[0]}, ${hobbies[1]} e ${hobbies[2]}`
-}
-
-function gerarDescricao(usuario){
-    console.log(`Olá, meu nome é ${usuario[0]}, tenho ${usuario[1]} anos, moro em ${usuario[2]} e meus hobbies são ${formatarHobbies(usuario[3])}`);
-}
-
+const prompt = require('readline-sync');
 const usuarios = [];
+function main(){
+    while(true){
+        const resposta = Number(prompt.question(`
+        Escolha uma opcao: \n
+        1. Criar usuario\n
+        2. Ver relatorio\n
+        3. Descrever usuario\n
+        4. Sair\n
+        `));
+        if(resposta === 1){
+        const name = prompt.question("Digite seu nome: ");
+        const age = prompt.question("Digite sua idade: ");
+        const city = prompt.question("Digite sua cidade: ");
+        const hobbie = prompt.question("Digite seus hobbie: ")
+        const hobbie2 = prompt.question("Digite seus hobbie: ")
+        const hobbie3 = prompt.question("Digite seus hobbie: ")
+        
+        makeUser(name, age, city, hobbie, hobbie2, hobbie3 );
+        
+        }else if(resposta === 2){
+            console.log(relatorio(usuarios));
+        }else if(resposta === 3){
+            let reposta = Number(prompt.question("Digite o numero do usuario que deseja descrever: "))
+            let i = reposta - 1;
+            if(i >= 0 && i < usuarios.length){
+                console.log(description(usuarios[i]));
+            }else{
+                console.log("indice invalido.");
+            }
+        }else if(resposta === 4){
+            console.log("Ok, até a proxima!");
+            break;
+        }else{
+            console.log("Resposta invalida, por favor digite '1', '2', '3' ou '4'.");
+    }
+    }
+}
+main()
 
-function adicionarUsuarios(usuarios, usuario){
+
+function makeUser(name, age, city, hobbie, hobbie2, hobbie3){
+    const user = [];
+    user.push(name);
+    user.push(age);
+    user.push(city);
+    const hobbies = [];
+    hobbies.push(hobbie);
+    hobbies.push(hobbie2);
+    hobbies.push(hobbie3);
+    const usuario = [...user, hobbies]
+    formatarHobbies(usuario);
+    addUser(usuarios, usuario);
+
+    console.log(`Usuario ${name} criado com sucesso!`);
+}
+
+function formatarHobbies(usuario){
+    const [ , , , hobbies] = usuario;
+    const hobbieFormatado =  `${hobbies[0]}, ${hobbies[1]} e ${hobbies[2]}`;
+    return hobbieFormatado;
+}
+
+function description(usuario){
+    const [name, age, city, hobbies] = usuario;
+    return `Olá, meu nome é ${name}, tenho ${age} anos, moro em ${city} e meus hobbies são ${formatarHobbies(usuario)}`
+}
+
+
+function addUser(usuarios, usuario){
     usuarios.push(usuario);
     return usuarios;
 }
 
-function gerarRelatório(usuarios){
+function relatorio(usuarios){
     const quantidadeUsuarios = usuarios.length;
-    console.log(`Total de usuários: ${quantidadeUsuarios}`);
+    console.log(`Quantidade de usuários: ${quantidadeUsuarios}`);
+    
+    for(let i = 0; i < usuarios.length; i++){
+        console.log(`${i + 1}. ${usuarios[i][0]}`);
+    }
 }
 
-let user1 = criarUsuario("William", 27, "São Leopoldo", ["futebol", "natação", "correr"]);
-let user2 = criarUsuario("Gabi", 23, "São Leopoldo", ["Acad", "Cross", "correr"]);
-let user3 = criarUsuario("Erick", 20, "São Leopoldo", ["gostar do Will", "basquete", "valorant"]);
 
-console.log(user1);
-
-formatarHobbies(user1[3]);
-console.log(formatarHobbies(user1[3]));
-
-gerarDescricao(user1);
-gerarDescricao(user2);
-gerarDescricao(user3);
-
-adicionarUsuarios(usuarios, user1);
-adicionarUsuarios(usuarios, user2);
-adicionarUsuarios(usuarios, user3);
-console.log(usuarios);
-
-gerarRelatório(usuarios);
 
