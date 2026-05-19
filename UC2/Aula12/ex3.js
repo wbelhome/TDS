c833f3ee6780429a624629718c3141285a7f8251
wbelhome <william.belhome99@gmail.com>
2026-05-18 21:02:23 -0300

    up

--- UC2/Aula12/ex3.js
+++ UC2/Aula12/ex3.js
@@ -14,16 +14,41 @@ function cadastrarAluno(nome, nota1, nota2, nota3){
 
 function calcularMedia(buscaNome, alunos){
     for(let i = 0; i < alunos.length; i++){
-        
+        if(alunos[i][0] === buscaNome){
+            const media = (alunos[i][1] + alunos[i][2] + alunos[i][3]) / 3
+            return media
+        }
     }
 }
 
-function verificarSituacao(){
-
+function verificarSituacao(media){
+    if(media >= 7){
+        return "Aprovado"
+    }
+    else if(media >= 5 && 5 < 7){
+        return "Recuperação"
+    }
+    else{
+        return "Reprovado"
+    }
 }
 
-function mostrarAlunos(buscaNome){
-    const media = calcularMedia(buscaNome, alunos)
+function mostrarAlunos(buscaNome, alunos){
+    for(let i = 0; i < alunos.length; i++){
+        if(alunos[i][0] === buscaNome){
+            const media = calcularMedia(buscaNome, alunos);
+            const situacao = verificarSituacao(media)
+            console.log(`Aluno: ${buscaNome} \n
+                Notas: ${alunos[i][1]}, ${alunos[i][2]} e ${alunos[i][3]} \n
+                Média: ${media.toFixed(2)} \n
+                Situação: ${situacao} \n
+                `)
+                
+        }
+        else{
+            console.log(`${buscaNome} não encontrado`)
+        }
+    }
 }
 
 function main(){
@@ -34,7 +59,7 @@ function main(){
         2- Mostrar alunos
         3- Sair \n`))
         if(resposta === 1){
-            const nome = prompt.question("Digite o nome do aluno: ")
+            const nome = prompt.question("Digite o nome do aluno: ").toLowerCase()
             const nota1 = Number(prompt.question("Digite a primeira nota: "))
             const nota2 = Number(prompt.question("Digite a segunda nota: "))
             const nota3 = Number(prompt.question("Digite a terceira nota: "))
@@ -42,7 +67,7 @@ function main(){
         }
         else if(resposta === 2){
             const buscaNome = prompt.question("Digite o nome do aluno: ").toLowerCase();
-            mostrarAlunos(buscaNome)
+            mostrarAlunos(buscaNome, alunos)
         }
         else if(resposta === 3){
             console.log("Até a próxima! ")
